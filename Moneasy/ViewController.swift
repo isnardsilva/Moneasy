a430903a6c3c0064fa4fedc03dca48f1a068b0ce
Isnard Silva <isnardsilva@icloud.com>
2021-01-02 09:43:25 -0300

    Create base project

---
+++ Moneasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneasy
+//
+//  Created by Isnard Silva on 02/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
